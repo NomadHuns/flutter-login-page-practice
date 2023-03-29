374fa1ee2dcc287280bc0d6a7542cbcc17d505bc
kimalss22 <kimalss22@gmail.com>
2023-03-29 10:07:25 +0900

    로그인 페이지 위젯 등록 완료

--- lib/views/pages/login/componunts/login_form.dart
+++ lib/views/pages/login/componunts/login_form.dart
@@ -5,6 +5,30 @@ class LoginForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        _loginInput("Email"),
+        _loginInput("Password"),
+        _loginButton()
+      ],
+    );
+  }
+
+  TextButton _loginButton() {
+    return TextButton(
+        onPressed: () {},
+        child: Text("Login"),
+      );
+  }
+
+  Widget _loginInput(String text) {
+    return Column(
+        children: [
+          Text(text),
+          TextFormField(
+            decoration: InputDecoration(hintText: "Enter $text"),
+          ),
+        ],
+      );
   }
 }
--- lib/views/pages/login/componunts/login_logo.dart
+++ lib/views/pages/login/componunts/login_logo.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class LoginLogo extends StatelessWidget {
   const LoginLogo({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        SvgPicture.asset("assets/logo.svg", height: 70, width: 70,),
+        Text("Login"),
+      ],
+    );
   }
 }
